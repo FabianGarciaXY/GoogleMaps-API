b9d8bad5ea165deca74206a8ad6df58e473c44d7
FabianGarciaXY <fabyan_0798@hotmail.com>
2022-05-15 14:15:28 -0500

    Proyecto inicializado con vue cli

--- src/main.js
+++ src/main.js
@@ -3,4 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// Aqui se agrego el Gmaps
+import VueGeolocation from "vue3-geolocation";
+import GMaps from "vuejs3-google-maps";
+
+createApp(App).use(store).use(router).use(VueGeolocation).use(GMaps, {
+    load: {
+      apiKey: "AIzaSyCZgr16JUc3aW1jeuYam3FaWHW89PfY08Q",
+      libraries: ["places"],
+    },
+  }).mount('#app')
